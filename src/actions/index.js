ff1d8c399abff28f2d2fc793dd106ee013e88e02
Tuan Bear <tuan.duong@enouvo.com>
2018-06-05 18:04:12 +0700

    add new actionTypes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
-import { INCREASE, DECREASE } from './type'
+import { INCREASE, DECREASE, STOP_COUNTER, FETCH_USER, CANCEL_FETCHING_USER, INCREASE_DONE, SHOW_LOADING_ICON, HIDE_LOADING_ICON, FETCH_USER_SUCCESS } from './type'
 
 export const counterIncrease = () => ({ type: INCREASE })
 export const counterDecrease = () => ({ type: DECREASE })
+export const stopCounter = () => ({ type: STOP_COUNTER })
+export const fetchUser = () => ({ type: FETCH_USER })
+export const cancelRequest = () => ({ type: CANCEL_FETCHING_USER })
+
+//export const doneIncrease = () => ({ type: INCREASE_DONE })
+//export const showLoadingIcon = () => ({ type: SHOW_LOADING_ICON })
+//export const hideLoadingIcon = () => ({ type: HIDE_LOADING_ICON })
+//export const fetchUserSuccess = () => ({ type: FETCH_USER_SUCCESS })
 
 //content actions
